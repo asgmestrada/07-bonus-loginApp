4ab159a9ecbd97f23116b3f695566889b4102b9a
moises <mestrada@asgbpm.com>
2020-04-04 23:39:57 -0600

    Carga de Proyecto

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+
+  usuario: UsuarioModel;
+  recordarme = false;
+  ngOnInit()
+  {
+    this.usuario = new UsuarioModel();
+  }
+  constructor( private auth: AuthService,
+               private router: Router ) { }
+
+
+
+  onSubmit( form: NgForm )
+  {
+    if ( form.invalid ) { return;
+  }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
+    this.auth.nuevoUsuario( this.usuario )
+      .subscribe( resp => {
+        console.log(resp);
+        Swal.close();
+
+        if ( this.recordarme )
+        {
+          localStorage.setItem('email', this.usuario.email);
+        }
+
+        this.router.navigateByUrl('/home');
+
+      }, (err) => {
+        console.log(err.error.error.message);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al autenticar',
+          text: err.error.error.message
+        });
+      });
+  }
+
+}
